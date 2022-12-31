4fb18cf1138e55b557ab9960b0282aa95d813d03
liuyuanbo <1131107655@qq.com>
2023-01-01 02:33:00 +0800

    'autoCommit'

--- vue-脚手架/vue-demo/src/App.vue
+++ vue-脚手架/vue-demo/src/App.vue
@@ -1,41 +1,32 @@
 <template>
-    <div>
-        <my-html></my-html>
-        <my-class></my-class>
-        <my-event></my-event>
-        <my-for></my-for>
-        <my-model></my-model>
-        <my-child></my-child>
-        <my-directive></my-directive>
-        <my-event></my-event>
-        <my-filter></my-filter>
-        <my-parent></my-parent>
-        <my-ref></my-ref>
-        <my-show></my-show>
-        <my-slot></my-slot>
-        <my-jsx></my-jsx>
-    </div>
+  <div>
+    <input type="text">
+  </div>
 </template>
 
 <script>
-import MyChild from './components/myChild.vue'
-import MyClass from './components/myClass.vue'
-import MyDirective from './components/myDirective.vue'
-import MyEvent from './components/myEvent.vue'
-import MyFilter from './components/myFilter.vue'
-import MyFor from './components/myFor.vue'
-// import myHtml from '@/components/myHtml.vue'
-import myHtml from './components/myHtml.vue'
-import MyJsx from './components/myJsx.vue'
-import MyModel from './components/myModel.vue'
-import MyParent from './components/myParent.vue'
-import MyRef from './components/myRef.vue'
-import MyShow from './components/myShow.vue'
-import MySlot from './components/mySlot.vue'
 export default {
-  components: { myHtml, MyClass, MyEvent, MyFor, MyModel, MyChild, MyDirective, MyFilter, MyParent, MyRef, MyShow, MySlot, MyJsx,  },
-  name:'App',
+name:'App',
+mounted(){
+  var myInput = document.querySelector('input')
+  myInput.onkeyup = fangdou(function(e){
+    console.log(this.value);
+    console.log(e.target.value);
+  },1000)
+  function fangdou(fn,time){
+    var timer = null
+    return function(){
+      if(timer){
+      clearTimeout(timer)
+    }
+    timer = setTimeout(()=>{
+      fn.apply(this,arguments)
+    },time)
+    }
+  }
 }
+}
+
 </script>
 
 <style>
