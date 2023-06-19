3af8f24e5d4391445d3f83496ed734b9ebee3a8f
liuyuanbo <1131107655@qq.com>
2023-06-19 11:05:59 +0800

    自定义组件的局部和全局组件

---
+++ vue/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <mybutton></mybutton>
+    </div>
+</template>
+
+<script>
+import mybutton from './mybutton.vue'
+export default{
+    components:{
+        mybutton
+    }
+}
+</script>
+<style>
+
+</style>
---
+++ vue/mybutton.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="box" @click="click1">困</div>
+        <div>困了{{ count }}次</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data (){
+        return {
+            count:1
+        }
+    },
+    methods:{
+        click1(){
+            this.count++
+        }
+    }
+}
+</script>
+
+<style>
+.box{
+    color: #bfa;
+}
+</style>
