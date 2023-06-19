efef1348629a574b3316ad19fb07b579250ee587
liuyuanbo <1131107655@qq.com>
2023-06-19 19:37:20 +0800

    静态页面搭建

---
+++ vue-脚手架/vue-demo/src/components/Main/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="todo-main">
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+    </ul>
+</template>
+
+<script>
+import Item from '@/components/Item'
+export default {
+name:'Main',
+components:{
+    Item
+}
+}
+</script>
+
+<style>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
