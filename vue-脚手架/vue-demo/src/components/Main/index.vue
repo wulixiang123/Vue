4d32884524b2ae6c8278a01f6f3d61847fb27856
liuyuanbo <1131107655@qq.com>
2023-06-20 16:08:14 +0800

    自定义事件与自定义todoList自定义事件

---
+++ vue-脚手架/vue-demo/src/components/Main/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul class="todo-main">
+
+    <Item
+      v-for="(todo, index) in todos"
+      :key="index"
+      :index="index"
+      :todo="todo"
+      @checkTodo="checkTodoHandler"
+      @deleteTodo="deleteTodoHandler"
+    ></Item>
+
+  </ul>
+</template>
+
+<script>
+import Item from '@/components/Item'
+export default {
+  name: "Main",
+  components: {
+    Item
+  },
+  props: ['todos'],
+  methods:{
+    checkTodoHandler(index){
+      this.$emit('checkTodo',index)
+    },
+    deleteTodoHandler(index){
+      this.$emit('deleteTodo',index)
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
