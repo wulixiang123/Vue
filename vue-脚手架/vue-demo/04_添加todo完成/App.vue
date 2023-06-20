15d4f3b76ddd79142c1d50b9b7faca1e079544b1
liuyuanbo <1131107655@qq.com>
2023-06-20 09:40:23 +0800

    添加todo完成

---
+++ vue-脚手架/vue-demo/04_添加todo完成/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <Header :add="add"></Header>
+      <Main :todos="todos"></Main>
+      <Footer :todos="todos"></Footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from'@/components/Header'
+import Main from '@/components/Main'
+import Footer from'@/components/Footer'
+import { nanoid } from 'nanoid'
+export default {
+name:'App',
+components:{Header,Main,Footer},
+data(){
+  return {
+    todos:[
+      {
+        id:nanoid(),
+        title:'吃饭',
+        isDone:true
+      },
+      {
+        id:nanoid(),
+        title:'睡觉',
+        isDone:true
+      },
+      {
+        id:nanoid(),
+        title:'敲代码',
+        isDone:false
+      },
+    ]
+  }
+},
+methods:{
+  add(title){
+    this.todos.unshift({
+      id:nanoid(),
+      title,
+      isDone:false
+    })
+  }
+}
+}
+</script>
+
+<style>
+/*app*/
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ vue-脚手架/vue-demo/04_添加todo完成/components/Main/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="todo-main">
+        <Item v-for="todo in todos" :key="todo.id" :todo="todo"></Item>
+    </ul>
+</template>
+
+<script>
+import Item from '@/components/Item'
+export default {
+name:'Main',
+components:{Item},
+props:['todos']
+}
+</script>
+
+<style>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
