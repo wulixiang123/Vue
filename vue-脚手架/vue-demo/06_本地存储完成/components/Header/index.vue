b48ab296c6926e5643bf76a903dc225c01141eae
liuyuanbo <1131107655@qq.com>
2023-06-20 14:15:43 +0800

    本地存储完成

---
+++ vue-脚手架/vue-demo/06_本地存储完成/components/Header/index.vue
@@ -0,0 +1,48 @@
+<template>
+        <div class="todo-header">
+          <input type="text" @keyup.enter="addTodo" placeholder="请输入你的任务名称，按回车键确认" />
+        </div>
+</template>
+
+<script>
+export default {
+    name:'Header',
+    props:['add','todos'],
+    methods:{
+      addTodo(e){
+        let text = e.target.value.trim()
+        if(!text){
+          alert('输入内容不能为空!!!')
+          return
+        }
+        let isRepeat = this.todos.some(item=>item.title==text)
+        if(isRepeat){
+          alert('输入的内容重复啦!!!')
+          e.target.value=''
+          return
+        }
+
+        this.add(text)
+        e.target.value=''
+      }
+    }
+}
+</script>
+
+<style>
+/*header*/
+.todo-header input {
+  width: 560px;
+  height: 28px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 4px 7px;
+}
+
+.todo-header input:focus {
+  outline: none;
+  border-color: rgba(82, 168, 236, 0.8);
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(82, 168, 236, 0.6);
+}
+</style>
---
+++ vue-脚手架/vue-demo/06_本地存储完成/components/Item/index.vue
@@ -0,0 +1,69 @@
+<template>
+            <li @mouseenter="isShow=true" @mouseleave="isShow=false">
+            <label>
+              <input type="checkbox" v-model="todo.isDone"/>
+              <span>{{todo.title}}</span>
+            </label>
+            <button class="btn btn-danger" v-show="isShow" @click="deleteTodo(index)">删除</button>
+          </li>
+</template>
+
+<script>
+export default {
+name:'Item',
+props:['todo','checkTodo','index','deleteTodo'],
+data(){
+  return {
+    isShow:false
+  }
+},
+methods:{
+  changeCheckedHandler(e) {
+      // e.target.checked 选中状态(不是.value)
+      // console.log(e.target.checked)
+
+      // (id写法)
+      // this.checkTodo(this.todo.id)
+      // 下标写法
+      this.checkTodo(this.index)
+    }
+}
+}
+</script>
+
+<style>
+/*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: right;
+  margin-top: 3px;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+
+</style>
