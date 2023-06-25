2358b51f790ebebf8f086a827d2c0f986d2b0c6f
liuyuanbo <1131107655@qq.com>
2023-06-25 11:50:55 +0800

    vuex3的安装与基本使用

---
+++ vue-脚手架/vue-demo/16_vuex@3的安装/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>App组件</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'App',
+    mounted(){
+        console.log(this.$store);
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-脚手架/vue-demo/16_vuex@3的安装/store/index.js
@@ -0,0 +1,10 @@
+// 1. 安装
+//  npm i vuex@3
+// 2. 引入使用
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+// 3. 创建并暴露(实例) - 需要传入一个配置对象,配置一会说
+export default new Vuex.Store({})
+// 4. 在main.js中引入进行关联
