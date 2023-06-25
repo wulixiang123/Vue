1fb1a2d6964401d7e5dfd9519704a990ee69dfc4
liuyuanbo <1131107655@qq.com>
2023-06-25 14:23:04 +0800

    userajax-vuex版本

---
+++ vue-脚手架/vue-demo/19_userajax-vuex版本/components/Header/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="jumbotron">
+    <h3 class="jumbotron-heading">Search Github Users</h3>
+    <div>
+      <input type="text" placeholder="enter the name you search" v-model="keyword"/>
+      <button @click="toSearch">Search</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      keyword: ''
+    }
+  },
+  methods: {
+    toSearch() {
+      this.$store.dispatch('getList', this.keyword)
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ vue-脚手架/vue-demo/src/components/Main/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row">
+    <h3 v-if="$store.state.isFirst">欢迎光临</h3>
+    <h3 v-else-if="$store.state.isLoading">加载中</h3>
+    <h3 v-else-if="$store.state.errorMessage">{{ $store.state.errorMessage }}</h3>
+    <div
+      v-else
+      class="card"
+      v-for="item in $store.state.list"
+      :key="item.id"
+    >
+      <a :href="item.html_url" target="_blank">
+        <img
+          :src="item.avatar_url"
+          style="width: 100px"
+        />
+      </a>
+      <p class="card-text">{{ item.username }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style scoped>
+</style>
