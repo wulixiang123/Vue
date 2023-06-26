7c2f43e4da8fa3814f22f470fe3e204f7f94d37b
liuyuanbo <1131107655@qq.com>
2023-06-26 10:59:58 +0800

    component : is使用

---
+++ vue-脚手架/vue-demo/src/components/mycomp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box">
+    <h3>mycomp</h3>
+  </div>
+</template>
+
+<script>
+export default {
+name:'mycomp'
+}
+</script>
+
+<style scoped>
+.box{
+padding: 40px 20px;
+background-color: hotpink;
+}
+</style>
