ada75ff0e2329f6d0d47667e467408a08ea66278
liuyuanbo <1131107655@qq.com>
2023-06-26 15:35:11 +0800

    插槽基础使用

---
+++ vue-脚手架/vue-demo/25_插槽基础/components/Child.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>Child组件</h3>
+    <slot>我是默认插槽</slot>
+    <hr>
+    <slot name="qwer">我是具名插槽</slot>
+    <hr>
+    <slot name="abc" :userinfo="userinfo" :intro="intro" msg="作用域插槽">作用域插槽</slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Child',
+    data(){
+        return{
+            userinfo:{
+                username:'悟空',
+                age:18
+            },
+            intro:'美猴王'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-脚手架/vue-demo/src/components/Child2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>Child2组件</h3>
+    <slot :userinfo="userinfo" :intro="intro">作用域插槽</slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Child2',
+    data(){
+        return{
+            userinfo:{
+                username:'八戒',
+                age:16
+            },
+            intro:'天蓬'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
