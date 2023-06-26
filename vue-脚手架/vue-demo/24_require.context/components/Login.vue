b8b3d8f6a6dd7d630d494665fbf4b52a4d68b0ec
liuyuanbo <1131107655@qq.com>
2023-06-26 13:57:37 +0800

    require.context的使用

---
+++ vue-脚手架/vue-demo/24_require.context/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h4>Login组件</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-脚手架/vue-demo/src/components/Detail.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h4>Detail组件</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Detail",
+}
+</script>
+
+<style scoped>
+
+</style>
