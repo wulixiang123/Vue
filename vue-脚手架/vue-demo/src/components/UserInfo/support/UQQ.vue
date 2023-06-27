d12fbeb8a8b65a74d8f351084d9507579dc2d16c
liuyuanbo <1131107655@qq.com>
2023-06-27 09:33:04 +0800

    自动化渲染

---
+++ vue-脚手架/vue-demo/src/components/UserInfo/support/UQQ.vue
@@ -0,0 +1,27 @@
+<template>
+  <div >
+        <div>
+          <span>关联平台</span>
+          <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-QQ"></use>
+          </svg>
+        </div>
+        <div class="num">账号:{{ row.qq }}</div>
+      </div>
+</template>
+
+<script>
+export default {
+    name:'UQQ',
+    props:['row']
+}
+</script>
+
+<style>
+.icon {
+  margin-left: 5px;
+}
+.num {
+  margin-left: 20px;
+}
+</style>
