630cbf39824d6875915eb3f2ce07997aea7184e6
liuyuanbo <1131107655@qq.com>
2023-06-27 10:04:40 +0800

    router安装

---
+++ vue-脚手架/vue-demo/27_router安装/router/index.js
@@ -0,0 +1,10 @@
+// 1. 安装
+//     npm i vue-router@3
+// 2. 引入使用
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+// 3. 创建并暴露(实例)
+export default new VueRouter()
+// 4. 在创建vm的时候进行关联
