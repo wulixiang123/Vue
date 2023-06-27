333a3b8b5c4b93f9ee7efa004e1401444f205254
liuyuanbo <1131107655@qq.com>
2023-06-27 15:06:40 +0800

    路由传参

---
+++ vue-脚手架/vue-demo/31_路由传参/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div role="tabpanel" class="tab-panel" id="About">
+      <div class="panel panel-default">
+        <div class="panel-body">Basic panel example</div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "About",
+  };
+  </script>
+  
+  <style scoped>
+  </style>
