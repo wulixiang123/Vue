8e39bb78a1ca99625e775a90607e677975806114
liuyuanbo <1131107655@qq.com>
2023-06-27 14:19:18 +0800

    三级路由使用

---
+++ vue-脚手架/vue-demo/src/pages/MsgDetail.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul class="list-group">
+        <li class="list-group-item">message01</li>
+        <li class="list-group-item">message02</li>
+        <li class="list-group-item">message03</li>
+        <li class="list-group-item">message04</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'MsgDetail'
+}
+</script>
+
+<style></style>
