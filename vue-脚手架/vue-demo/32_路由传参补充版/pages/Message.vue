6b5818890da225901af2666f5651381c98f6d657
liuyuanbo <1131107655@qq.com>
2023-06-27 15:28:41 +0800

    路由传参

---
+++ vue-脚手架/vue-demo/32_路由传参补充版/pages/Message.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- Tab panes -->
+  <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="Message">
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="item in list" :key="item.id">
+                        <router-link :to=" '/home/message/msgdetail/' + item.id + '?name=' + item.name ">{{ item.name }}</router-link>
+                    </li>
+                </ul>
+                <div class="alert alert-success" role="alert">
+                    <router-view></router-view>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name:'Message',
+    data(){
+        return{
+            list:[
+                {id:1,name:'悟空'},
+                {id:2,name:'八戒'},
+                {id:3,name:'沙僧'}
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-脚手架/vue-demo/src/pages/NewsDetail.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="list-group">
+          <li class="list-group-item">新闻id:{{ $route.params.newId }}</li>
+          <li class="list-group-item">新闻内容:{{ $route.query.title }}</li>
+        </ul>
+</template>
+
+<script>
+export default {
+    name:'NewsDetail'
+}
+</script>
+
+<style scoped>
+
+</style>
