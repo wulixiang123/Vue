bf58b0b18cd47b0671946a7886e89942895979b7
liuyuanbo <1131107655@qq.com>
2023-06-30 18:48:44 +0800

    复习

--- vue-脚手架/vue-demo/38_复习/App.vue
+++ vue-脚手架/vue-demo/38_复习/App.vue
@@ -12,6 +12,8 @@
         <my-parent></my-parent>
         <my-ref></my-ref>
         <my-show></my-show>
+        <my-slot></my-slot>
+        <my-jsx></my-jsx>
     </div>
 </template>
 
@@ -24,12 +26,14 @@ import MyFilter from './components/myFilter.vue'
 import MyFor from './components/myFor.vue'
 // import myHtml from '@/components/myHtml.vue'
 import myHtml from './components/myHtml.vue'
+import MyJsx from './components/myJsx.vue'
 import MyModel from './components/myModel.vue'
 import MyParent from './components/myParent.vue'
 import MyRef from './components/myRef.vue'
 import MyShow from './components/myShow.vue'
+import MySlot from './components/mySlot.vue'
 export default {
-  components: { myHtml, MyClass, MyEvent, MyFor, MyModel, MyChild, MyDirective, MyFilter, MyParent, MyRef, MyShow, },
+  components: { myHtml, MyClass, MyEvent, MyFor, MyModel, MyChild, MyDirective, MyFilter, MyParent, MyRef, MyShow, MySlot, MyJsx,  },
   name:'App',
 }
 </script>
--- vue-脚手架/vue-demo/38_复习/components/myModel.vue
+++ vue-脚手架/vue-demo/38_复习/components/myModel.vue
@@ -12,11 +12,25 @@ data(){
 },
 // <input v-model="keyword"/>
 // <input :value="keyword" @input="keyword = $event.target.value"/>
+// 期望: 期望的是双向数据绑定
+//      数据发生改变,页面发生变化;  页面发生变化,数据也发生变化
 render(createElement){
     return createElement('input',{
-        attrs:{
-            value:this.keyword
-        },
+            // attrs
+            // 我们发现,当页面发生变化的时候,走了input事件,此时改变可keyword的内容
+            // 当我们在开发者工具中修改 keyword 的时候,发生input框上的value属性中的内容变化了
+            //    先改的input框内容,devtools再改数据,input不更新
+            //    发现使用 attrs 属性设置value有问题,不要用这个
+            // <input value="" />
+            // attrs: {
+            //     value: this.keyword
+            // },
+            // -------------
+            // 去使用 domProps 中的 value 去设置
+            // document.getElementById('input').value = 'xxx'
+            domProps:{
+                value:this.keyword
+            },
         on:{
             input:(e)=>{
                 this.keyword = e.target.value
