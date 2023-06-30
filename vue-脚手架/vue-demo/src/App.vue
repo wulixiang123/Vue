cca183377d88ba1a442f1ba616e47a16d57150f2
liuyuanbo <1131107655@qq.com>
2023-06-30 10:32:53 +0800

    更新

--- vue-脚手架/vue-demo/src/App.vue
+++ vue-脚手架/vue-demo/src/App.vue
@@ -12,6 +12,7 @@
         <my-parent></my-parent>
         <my-ref></my-ref>
         <my-show></my-show>
+        <my-slot></my-slot>
     </div>
 </template>
 
@@ -28,8 +29,9 @@ import MyModel from './components/myModel.vue'
 import MyParent from './components/myParent.vue'
 import MyRef from './components/myRef.vue'
 import MyShow from './components/myShow.vue'
+import MySlot from './components/mySlot.vue'
 export default {
-  components: { myHtml, MyClass, MyEvent, MyFor, MyModel, MyChild, MyDirective, MyFilter, MyParent, MyRef, MyShow, },
+  components: { myHtml, MyClass, MyEvent, MyFor, MyModel, MyChild, MyDirective, MyFilter, MyParent, MyRef, MyShow, MySlot,  },
   name:'App',
 }
 </script>
